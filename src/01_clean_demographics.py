5a2245b6e8e119f6e91bfb39c48043f31c919fae
Sreshta Yelisetti <sreshta.yelisetti@berkeley.edu>
2025-10-17 02:03:15 -0700

    add combined demographics dataset

--- src/01_clean_demographics.py
+++ src/01_clean_demographics.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 # load the raw NHANES  file
-demo = pd.read_sas("data/raw/DEMO_J.XPT")  # path to your raw XPT file
+demo_2017 = pd.read_sas("data/raw/demo_2017_2018.xpt")
+demo_2015 = pd.read_sas("data/raw/demo_2015_2016.xpt") # path to your raw XPT file
+
+# make a column for the cycle
+demo_2017["Cycle"] = "2017-2018"
+demo_2015["Cycle"] = "2015-2016"
 
 # only the variables you want
 keep_vars = [
@@ -11,13 +16,27 @@ keep_vars = [
     "RIDRETH1",   # Race/ethnicity
     "WTMEC2YR",   # MEC exam weight
     "SDMVPSU",    # Pseudo-PSU
-    "SDMVSTRA"    # Pseudo-stratum
+    "SDMVSTRA",   # Pseudo-stratum
+    "Cycle"
 ]
 
-# create a subset datafram with only the variables you want
-demo_subset = demo[keep_vars]
+#vertically concatenate
+demo_all = pd.concat([demo_2015, demo_2017], axis=0, ignore_index=True)
+
+# create a subset dataframe with only the variables you want
+demo_subset = demo_all[keep_vars]
+
+#rename columns
+df = demo_subset.rename(columns = {
+    "RIDAGEYR": "Age",
+    "RIAGENDR": "Sex",
+    "RIDRETH1": "Race/Ethnicity",
+    "WTMEC2YR": "MEC exam weight",
+    "SDMVPSU": "Pseudo PSU",
+    "SDMVSTRA": "Pseudo stratum"
+})
 
 # Save the cleaned dataset
-demo_subset.to_csv("data/interim/demo_subset.csv", index=False)
+df.to_csv("data/interim/demo_subset.csv", index=False)
 
 print("Demographics data subset saved successfully!")
